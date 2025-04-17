3a76a2090b152f618234a0a3c9e4b4601f09726c
javierjericm <javierjericm@gmail.com>
2025-04-17 21:40:00 +0800

    String data types

---
+++ program-simulation/String_Chars/String_Chars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jeric";
+            char letter = 'a';
+
+            Console.Write("My name is ");
+            Console.Write(name);
+
+            Console.WriteLine();
+            Console.WriteLine(letter);
+
+            Console.ReadLine();
+        }
+    }
+}
