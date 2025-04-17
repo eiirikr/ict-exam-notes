d986847d84cf7f07d1aa73eddbc82a200a94344c
javierjericm <javierjericm@gmail.com>
2025-04-17 22:45:59 +0800

    Var keyword

---
+++ program-simulation/Var/Var/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = -22;
+            Console.WriteLine(age);
+
+            var bigNumber = 9000000000L;
+            Console.WriteLine(bigNumber);
+
+            var negative = -55.2D;
+            Console.WriteLine(negative);
+
+            var precision = 5.0000001F;
+            Console.WriteLine(precision);
+
+            var money = 34.99M;
+            Console.WriteLine(money);
+
+            var name = "Jeric";
+            Console.WriteLine(name);
+
+            var letter = 'j';
+            Console.WriteLine(letter);
+
+            Console.ReadLine();
+        }
+    }
+}
