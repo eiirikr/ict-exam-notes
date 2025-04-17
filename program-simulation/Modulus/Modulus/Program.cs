6bd9f98ae36f2787b3ee23d17a3caf5da43f17f8
javierjericm <javierjericm@gmail.com>
2025-04-17 22:36:58 +0800

    Remainder

---
+++ program-simulation/Modulus/Modulus/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = 10;
+            int secondNum = 3;
+
+            Console.WriteLine(firstNum / secondNum);
+            Console.WriteLine(firstNum % secondNum);
+
+            Console.ReadLine();
+        }
+    }
+}
