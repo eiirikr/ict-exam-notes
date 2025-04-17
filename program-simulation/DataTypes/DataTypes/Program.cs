0f83b06acc440072b582a650c2c840618e084704
javierjericm <javierjericm@gmail.com>
2025-04-17 21:31:31 +0800

    Data Types (numbers)

--- program-simulation/DataTypes/DataTypes/Program.cs
+++ program-simulation/DataTypes/DataTypes/Program.cs
@@ -10,11 +10,38 @@ namespace DataTypes
     {
         static void Main(string[] args)
         {
-            int age = 22;
+            /*int x = 10;
+            int y = 20;
+            int z = 30;*/
+
+            /*int x = 10, 
+                y = 20, 
+                z = 30;*/
+
+            int age = -22;
             Console.WriteLine(age);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
 
             long bigNumber = 9000000000L;
             Console.WriteLine(bigNumber);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+
+            double negative = -55.2D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            float precision = 5.0000001F;
+            Console.WriteLine(precision);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+
+            decimal money = 34.99M;
+            Console.WriteLine(money);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
 
             Console.ReadLine();
         }
