9909d8be74f2aebb1b3e208b58a20604a0000aff
javierjericm <javierjericm@gmail.com>
2025-04-18 13:19:16 +0800

    Console Input/Output

---
+++ program-simulation/ConsoleIO/ConsoleIO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello my name is Jeric!");
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Your name is " + name + " and you're " + age + " years old.");
+
+            Console.ReadLine();
+        }
+    }
+}
