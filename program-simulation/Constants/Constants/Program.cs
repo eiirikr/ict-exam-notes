f301296d608f8fa330b156c5c5d6b35cfb27ef2e
javierjericm <javierjericm@gmail.com>
2025-04-18 11:13:51 +0800

    Constants

---
+++ program-simulation/Constants/Constants/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int vat = 20;
+            const double percentVAT = vat / 100D;
+
+            int balance = 1000;
+
+            Console.WriteLine(balance * (vat / 100D));
+            Console.WriteLine(balance * percentVAT);
+
+            Console.WriteLine(vat);
+
+            Console.ReadLine();
+        }
+    }
+}
