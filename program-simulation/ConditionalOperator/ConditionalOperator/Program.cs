6577eedcde0583a6966dd311486e9bcd2b2e560b
javierjericm <javierjericm@gmail.com>
2025-04-18 23:38:10 +0800

    Conditional Operator

---
+++ program-simulation/ConditionalOperator/ConditionalOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            string result = age >= 0 ? "Valid" : "Invalid";
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
