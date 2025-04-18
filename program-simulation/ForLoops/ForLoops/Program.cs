d05c9bca2ee62618547ec62fd6ee6bc6802a8095
javierjericm <javierjericm@gmail.com>
2025-04-18 21:03:53 +0800

    For loops

---
+++ program-simulation/ForLoops/ForLoops/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a message: ");
+            string message = Console.ReadLine();
+
+            if (number <= 0)
+            {
+                Console.WriteLine("Sorry, please enter a value above 0.");
+            }
+            else
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    Console.WriteLine(message);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
