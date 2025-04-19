bfb60468443a4bd88fe542fe93738f316b86af0b
javierjericm <javierjericm@gmail.com>
2025-04-19 13:17:41 +0800

    Verbatim string literal (@)

---
+++ program-simulation/Output/Output/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // \t \n \" \\ = \ (scape character)
+            string speech = "He said \"something\"";
+            string path = "C:\\Users\\Jessa Mae\\OneDrive\\Desktop\\ict-exam-notes\nNew line test";
+            Console.WriteLine(path);
+            Console.WriteLine(speech);
+            // $ +
+            // $"Your name is {name}"
+            // "Your name is " + name
+
+            path = @"C:\\Users\\Jessa Mae\\OneDrive\\Desktop\\ict-exam-notes" + "\nNew line test";
+            Console.WriteLine(path);
+
+            string name = @"Hello ""someone""";
+            Console.WriteLine(name);
+
+            name = "Hello 'someone'";
+            Console.WriteLine(name);
+
+            Console.ReadLine();
+        }
+
+    }
+}
