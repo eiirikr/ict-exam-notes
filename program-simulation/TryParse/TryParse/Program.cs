f2ae39872e7fb2dffd75d5efc374a0780dd4ae57
javierjericm <javierjericm@gmail.com>
2025-04-19 12:06:18 +0800

    TryParse Function

---
+++ program-simulation/TryParse/TryParse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool success = true;
+
+            while (success)
+            {
+                Console.Write("Enter a number: ");
+
+                if (int.TryParse(Console.ReadLine(), out int num))
+                {
+                    success = false;
+                    Console.WriteLine(num);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
