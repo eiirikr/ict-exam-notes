6fa3f99eb16959e3aaf9e525a0cbde831efb3cfc
javierjericm <javierjericm@gmail.com>
2025-04-19 14:13:35 +0800

    Composive formatting ({0})

---
+++ program-simulation/StringFormatting/StringFormatting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFormatting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jeric";
+            int age = 22;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Name: " + name + "\nAge: " + age);
+
+            Console.WriteLine("Your name is " + name + ", and your age is " + age);
+
+            Console.WriteLine("Your name is {0}, and your age is {1}", name, age);
+            Console.WriteLine("Name: {0}\nAge: {1}", name, age);
+
+            Console.ReadLine();
+        }
+    }
+}
