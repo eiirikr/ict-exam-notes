c0c809d91a7151c98ed1333622fe4722704441c1
javierjericm <javierjericm@gmail.com>
2025-04-19 14:19:32 +0800

    String Interpotion ($)

---
+++ program-simulation/StringInterpotion/StringInterpotion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInterpotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jeric";
+            int age = 22;
+
+            Console.WriteLine($"Your name is {name} and your age is {age}");
+
+            Console.ReadLine();
+        }
+    }
+}
