c70f9827e46f595fc54934f6797e467114e9dc4a
javierjericm <javierjericm@gmail.com>
2025-04-20 22:28:46 +0800

    String isNullOrEmpty function

---
+++ program-simulation/IsNullOrEmpty/IsNullOrEmpty/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNullOrEmpty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name}");
+
+            /*if (name != "")
+                Console.WriteLine("0");*/
+
+            /*if (!name.Equals(""))
+                Console.WriteLine("1");*/
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                if (name.Equals("Jeric"))
+                {
+                    Console.WriteLine("Correct");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
