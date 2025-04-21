b6e185ea7789cdceebbb25aa423c0e2467efdb26
javierjericm <javierjericm@gmail.com>
2025-04-21 23:57:41 +0800

    Arrays[]

---
+++ program-simulation/Arrays/Arrays/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int num1 = 5;
+            int num2 = 10;
+            int num3 = 15;*/
+
+            /*Console.Write("Enter a number: ");
+            numbers[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a number: ");
+            numbers[1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a number: ");
+            numbers[2] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{num1} {num2} {num3}");
+            Console.WriteLine($"{numbers[0]} {numbers[1]} {numbers[2]}");*/
+
+            int[] numbers = new int[3];
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Enter a number: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+
+            Console.WriteLine();
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
