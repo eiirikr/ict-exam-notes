924038cc743440dd4143471643323d93c51efdb0
javierjericm <javierjericm@gmail.com>
2025-04-27 23:53:41 +0800

    Angle Exercise (Array)

---
+++ program-simulation/AngleExercise/AngleExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngleExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int angleCount = 3;
+            int[] angles = new int[angleCount];
+
+            for (int i = 0; i < angles.Length; i++)
+            {
+                Console.Write($"Enter angle {i + 1}: ");
+                angles[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int angleSum = 0;
+
+            foreach (int angle in angles)
+            {
+                angleSum += angle;
+            }
+
+            Console.WriteLine(angleSum == 180 ? "Valid" : "Invalid!") ;
+
+            Console.ReadLine();
+        }
+    }
+}
